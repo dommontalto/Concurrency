d526f70affb6288448447bd832d776451c0bcb52
Dom Montalto <dommontalto@icloud.com>
2025-03-13 06:53:06 +0000

    Task modifier

--- Concurrency/Modules/Tasks/TasksModule.swift
+++ Concurrency/Modules/Tasks/TasksModule.swift
@@ -9,44 +9,69 @@ import SwiftUI
 
 struct TasksModule: View {
     @StateObject var viewModel = AsyncAwaitViewModel()
-    @State private var task: Task<(), any Error>?
+    @State private var selectedUserId: String?
+    @State private var didExecuteTask = false
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(viewModel.users) { user in
-                    VStack(alignment: .leading) {
-                        Text(user.username)
-                        
-                        if viewModel.isUpdating {
-                            ProgressView()
-                        } else {
-                            Text(user.email)
-                        }
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(viewModel.users) { user in
+                        NavigationLink(value: user, label: {
+                            VStack(alignment: .leading) {
+                                Text(user.username)
+                                
+                                if viewModel.isUpdating {
+                                    ProgressView()
+                                } else {
+                                    Text(user.email)
+                                }
+                            }
+                        })
+//                        .onTapGesture {
+//                            selectedUserId = user.id
+//                        }
                     }
                 }
             }
-        }
-        .onDisappear() {
-            self.task?.cancel()
-        }
-        .onAppear() {
-            self.task = Task(priority: .high){
-                print("Started High Priority Task")
+            .task(id: selectedUserId) {
+                print("Update user email now: \(selectedUserId ?? "")")
+            }
+            .task {
+                guard !didExecuteTask else {
+                    print("Task already ran..")
+                    return
+                }
+                print("Running fetch data task..")
                 await viewModel.fetchData()
-                print("Finished High Priority Task")
+                didExecuteTask = true
             }
-            
-            Task(priority: .low){
-                print("Started Low Priority Task")
-//                await viewModel.fetchData()
-            }
-            
-            Task(priority: .background){
-                print("Started Background Priority Task")
-//                await viewModel.fetchData()
-            }
+            .navigationDestination(for: User.self, destination: { user in
+                Text(user.username)
+            })
         }
+//        .onDisappear {
+//            task?.cancel()
+//        }
+//        .onAppear {
+//            self.task = Task(priority: .high) { // 1
+//                print("Started task 1..")
+//                await viewModel.fetchData()
+//                // do some subsequent task..
+//                print("Completed task 1..")
+//                print("Users count \(viewModel.users.count)")
+//            }
+//
+//            Task(priority: .low) { // 2
+//                // await doSomething
+//                print("Started task 2..")
+//            }
+//
+//            Task(priority: .background) { // 3
+//                // await doSomething
+//                print("Started task 3..")
+//            }
+//        }
     }
 }
 
